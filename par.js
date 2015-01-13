75864649c1c63a644714ed4dc96b9e577430aff8
Arkadiusz <arkadiusz.heblinski@gmail.com>
2015-01-13 10:05:33 +0100

    zmiana parametrów

--- par.js
+++ par.js
@@ -9,21 +9,21 @@
 		WEB_PORT : 8888,
 		WEB_DIR : dir + "/build",
 		// WEB_PROXY : {host: '127.0.0.1', port: 80, path: '/dane.php' },
-		WEB_PAR_FTP : true, //false,//true,
+		WEB_PAR_FTP : false, //false,//true,
 		WEB_SYGN_FTP : false,//true,//false,
-		WEB_KOM_FTP : true,
-		WEB_REFRESH : 10000,
+		WEB_KOM_FTP : false,
+		WEB_REFRESH : 15000,
 		// GIVE_TIME : false,
 		// FTP_CACHE_DIR : __dirname + "/ftp",
 		NODE_MAIN : dir + "/node/main.js",
 		NODE_DIAGN_PORT : 8889,
 		LOG_DIR : dir + "/log",
-		// LOGGER_DIR : "USB",
-		LOGGER_DIR : "D:/Rejestracja",
+		LOGGER_DIR : "USB",
+		// LOGGER_DIR : "D:/Rejestracja",
 		PARAM_LOC_FILE : dir + "/default.json",
 		// NTP_HWCLOCK : true,
 		// NTP_IPs : ["192.168.3.51", "ntp1.tp.pl"],		//tablica priorytetowa z serwerami czasu
-		WER_NODE	: "1.10.12",
+		WER_NODE	: "1.14.10",
 		ADM_WEB_DIR : dir + "/adm"
 	};
 }());
